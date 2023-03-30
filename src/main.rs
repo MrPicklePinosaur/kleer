498444ee758709d35f9559c5dfeffead9eb9d78a
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-03-30 14:30:22 -0400

    (master) basic clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod clear_screen;
+
+use clap::{Parser, ValueEnum};
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum ClearMode {
+    Basic,
+}
+
+impl Default for ClearMode {
+    fn default() -> Self {
+        Self::Basic
+    }
+}
+
+#[derive(Parser)]
+#[command(name = "kuuhaku")]
+#[command(bin_name = "kuuhaku")]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[arg(long, short)]
+    clear_mode: ClearMode,
+}
+
 fn main() {
-    println!("こんにちは世界！");
+    let cli = Cli::parse();
+
+    match cli.clear_mode {
+        ClearMode::Basic => clear_screen::basic(),
+    }
 }
